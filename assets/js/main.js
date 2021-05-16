237185721a66412994f8a54f7445b7063d687a6b
상민 박 <smpark95@mindslab.ai>
2021-05-16 17:41:06 -0400

    Add animation to mobile nav header.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,21 @@
 /*===== MENU SHOW =====*/
 
-const showMenu = (toggleId, navId) => {
-  const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId);
+$('#nav-toggle').on('click', function () {
+  $('#nav-menu').toggleClass('show');
+  $('body').toggleClass('blur');
 
-  if (toggle && nav) {
-    toggle.addEventListener('click', () => {
-      nav.classList.toggle('show');
-    });
-  }
-};
-showMenu('nav-toggle', 'nav-menu');
+  $('.blur .l-main').one('click', function () {
+    $('#nav-menu').removeClass('show');
+    $('body').removeClass('blur');
+  });
+
+  $('.blur .nav__link').one('click', function () {
+    $('#nav-menu').removeClass('show');
+    $('body').removeClass('blur');
+  });
+});
+
+// showMenu('nav-toggle', 'nav-menu');
 
 /*===== ACTIVE AND REMOVE MENU =====*/
 const navLink = document.querySelectorAll('.nav__link');
